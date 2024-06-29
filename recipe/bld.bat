ba1ce2358a4ef982bd2c39f3100587b3b9d10917
Rafael Martins <rmartins@anaconda.com>
2024-06-29 03:32:01 +0200

    win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -37,8 +37,6 @@ cmake -S"%SRC_DIR%/%PKG_NAME%" -Bbuild -GNinja ^
     -DQT_FEATURE_sql_mysql=OFF ^
     -DQT_FEATURE_sql_psql=ON ^
     -DQT_FEATURE_harfbuzz=OFF ^
-    -DQT_FEATURE_system_sqlite=ON ^
-    -DQT_FEATURE_system_zlib=ON ^
     -DQT_FEATURE_enable_new_dtags=OFF ^
     -DINPUT_opengl=%OPENGLVER%
 if errorlevel 1 exit 1
