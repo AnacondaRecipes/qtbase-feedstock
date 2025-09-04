ea2b1223a6f74f8d0bef55789bbeca4a09d1e7d1
Brian Wing <bwing@anaconda.com>
2025-09-03 20:48:24 -0400

    Fix QMake builds on OSX

--- recipe/run_qt_test.sh
+++ recipe/run_qt_test.sh
@@ -13,7 +13,14 @@ if [ "$(uname)" = "Linux" ]; then
         dbus-uuidgen --ensure
     fi
 
+    # Force GLVND to load our mesa libs.
+    export __EGL_VENDOR_LIBRARY_FILENAMES=${PREFIX}/share/glvnd/egl_vendor.d/99_anaconda_mesa.json
+
     xvfb-run -a --server-args="-screen 0 1024x768x24" ctest --test-dir build --verbose
 else
-    ctest --test-dir build --output-on-failure
+    # To use the local xvfb-run script.
+    export PATH=${PWD}:${PATH}
+
+    # TODO: Can't run these in PBP yet.
+#    xvfb-run -a --server-args="-screen 0 1024x768x24" ctest --test-dir build --verbose
 fi
